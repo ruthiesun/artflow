6c32f6c4bdcb2cf87e2c03efea55952e7c646dee
Ruthie Sun <ruthie.sun@gmail.com>
2025-10-14 21:26:14 -0700

    improved ui of image create/edit forms

--- frontend/src/components/business/ImageModal.tsx
+++ frontend/src/components/business/ImageModal.tsx
@@ -68,10 +68,15 @@ export function EditImageModal({ editingImage, setImages, images, onClose }: Edi
         <SmallModal content={
             (
                 <div>
-                    {error && <p>{error}</p>}
-                    <Input label="Url" type="text" value={url} setValue={setUrl} />
-                    <TextAreaInput label="Caption" value={caption} setValue={setCaption} />
-                    <DateInput label="Date" value={date} setValue={setDate} />
+                    <div className="mb-2">
+                        <Input label="Url" type="text" value={url} setValue={setUrl} />
+                    </div>
+                    <div className="mb-2">
+                        <TextAreaInput label="Caption" value={caption} setValue={setCaption} />
+                    </div>
+                    <div className="mb-2">
+                        <DateInput label="Date" value={date} setValue={setDate} />
+                    </div>
                     {error && <ErrorText className="mb-4" content={error} />}
                     <PrimaryButton type='button' text="Update" disabled={url.trim() === ""} onClick={updateImage} />
                 </div>
@@ -92,14 +97,35 @@ export function AddImageModal({ projectName, setImages, images, onClose }: AddIm
     const [date, setDate] = useState<string>("");
     const [caption, setCaption] = useState<string>("");
     const [error, setError] = useState<string | null>(null);
+    const [validator, setValidator] = useState<Validator>();
+    const nav = useNavigate();
+
+    useEffect(() => {
+        Validator.getInstance()
+            .then((res) => {
+                setValidator(res);
+            })
+            .catch((err) => {
+                navToErrorPage({ nav, err });
+            });
+    }, []);
 
     const updateImages = (() => {
-        if (!url) {
-            setError("URL must be nonempty")
-            return
+        if (validator == undefined) {
+            return;
         }
 
-        const dateTime: string | null = date === "" ? null : (new Date(date + "T00:00:00")).toISOString();
+        if (!(new RegExp(validator.getProjectImageUrlRegex()).test(url))) {
+            setError(validator.getProjectImageUrlMessage());
+            return;
+        }
+
+        if (!(new RegExp(validator.getProjectImageCaptionRegex()).test(caption))) {
+            setError(validator.getProjectImageCaptionMessage());
+            return;
+        }
+
+        const dateTime: string | null = date === "" ? null : date + "T00:00:00";
 
         const newImage: ProjectImagePrePersist = { position: images.length, caption: caption, dateTime: dateTime, url: url, projectName: projectName }
         setImages([...images, newImage]);
@@ -109,11 +135,17 @@ export function AddImageModal({ projectName, setImages, images, onClose }: AddIm
     return (
         <SmallModal content={(
             <div>
-                {error && <p>{error}</p>}
-                <Input label="Url" type="text" value={url} setValue={setUrl} />
-                <TextAreaInput label="Caption" value={caption} setValue={setCaption} />
-                <DateInput label="Date" value={date} setValue={setDate} />
-                <PrimaryButton type='button' text="Create" disabled={url.trim() === ""} onClick={updateImages} />
+                <div className="mb-2">
+                    <Input label="Url" type="text" value={url} setValue={setUrl} />
+                </div>
+                <div className="mb-2">
+                    <TextAreaInput label="Caption" value={caption} setValue={setCaption} />
+                </div>
+                <div className="mb-2">
+                    <DateInput label="Date" value={date} setValue={setDate} />
+                </div>
+                {error && <ErrorText className="mb-4" content={error} />}
+                <PrimaryButton type='button' text="Create" disabled={url.trim() === "" || validator == undefined} onClick={updateImages} />
             </div>
         )} onClose={onClose} />
     )
